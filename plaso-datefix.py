fa6aa7ef93bd64ee922d87af36c877ca4fd8dc77
Jim Clausing <clausing@computer.org>
2017-03-04 13:36:20 -0500

    Add description to ArgumentParser

--- plaso-datefix.py
+++ plaso-datefix.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python
 #
-# Massage plaso l2tcsv output to use ISO(-ish) datetimestamp
+# Massage plaso l2tcsv output to use ISO-8601(-ish) datetimestamp
 # 
 # Author: Jim Clausing
-# Date: 2017-02-15
-# Version: 0.2
+# Date: 2017-03-04
+# Version: 0.3
 
 import sys
 import argparse
@@ -16,7 +16,7 @@ __version__ = ".".join(map(str, __version_info__))
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description='Massage plaso l2tcsv output to use ISO-8601(-ish) datetimestamp')
     parser.add_argument("files", metavar='FILE', nargs='*', help='files to manipulate, if empty, use stdin')
     parser.add_argument('-V','--version', action='version', help='print version number', 
             version='%(prog)s v' + __version__)

598cfb6335f350e3bc0b73504bf781f620ec3406
Jim Clausing <clausing@computer.org>
2023-10-11 13:24:18 -0400

    [20231011-000] Fix env for MacOS, fix usage string for systems that don't like %prog

--- le-hex-to-ip.py
+++ le-hex-to-ip.py
@@ -1,17 +1,18 @@
-#!/bin/env python3
+#!/usr/bin/env python3
 
 __description__ = "Program to convert little-endian hex (as found in /proc/<pid>/net/tcp[6], etc.) to IPv4 or IPv6 addresses"
 __author__ = 'Jim Clausing'
-__version__ = '1.1'
-__date__ = '2023-10-05'
+__version__ = '1.1.1'
+__date__ = '2023-10-11'
 
+import os
 import struct
 import socket
 import argparse
 
 def main():
     
-    parser = argparse.ArgumentParser(usage='usage: %prog expression\n' + __description__)
+    parser = argparse.ArgumentParser(usage='usage: ' + os.path.basename(__file__) + ' hexstring\n' + __description__)
     parser.add_argument("hexstring", metavar='HEXSTRING', nargs='+', help='hexstrings to convert')
     parser.add_argument('-V','--version', action='version', help='print version number', 
                         version='%(prog)s v' + __version__)

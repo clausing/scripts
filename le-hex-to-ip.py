93042b36372109fdbdfefb58d622893c83013110
Jim Clausing <clausing@computer.org>
2024-03-19 22:49:22 +0000

    [20240319-002] Update le-hex-to-ip.py to use inet_ntop instead of manually doing the IPv6

--- le-hex-to-ip.py
+++ le-hex-to-ip.py
@@ -2,8 +2,8 @@
 
 __description__ = "Program to convert little-endian hex (as found in /proc/<pid>/net/tcp[6], etc.) to IPv4 or IPv6 addresses"
 __author__ = 'Jim Clausing'
-__version__ = '1.1.1'
-__date__ = '2023-10-11'
+__version__ = '1.2.0'
+__date__ = '2024-03-19'
 
 import os
 import struct
@@ -21,17 +21,13 @@ def main():
     for hexstr in args.hexstring:
         if len(hexstr) == 8:
             addr_long = int(hexstr, 16)
-            print (socket.inet_ntoa(struct.pack("<L", addr_long)) )
+            print (socket.inet_ntop(socket.AF_INET,struct.pack("<L", addr_long)) )
         elif len(hexstr) == 32:
             out = []
             outstr = ''
             for i in range(0, len(hexstr), 8):
-                out.append( struct.pack("<L", int(hexstr[i:i+8],16)).hex() )
-            for i in range(0, len(out)):
-                outstr += out[i][:4] + ':' + out[i][4:]
-                if i < 3:
-                 outstr += ':' 
-            print (outstr)
+                outstr += struct.pack("<L", int(hexstr[i:i+8],16)).hex()
+            print (socket.inet_ntop(socket.AF_INET6,bytes.fromhex(outstr)))
                 
 
 
